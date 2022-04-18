11e33dc88e136473cb96fd92d4b6f18c5c3887b2
yanming <254500312@qq.com>
2022-04-18 22:46:41 +0800

    寻找两个正序数组的中位数, 困难

---
+++ leetcode/findMedianSortedArrays/main.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums1 := []int{1, 3, 4, 9}
+	nums2 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	fmt.Println(findMedianSortedArrays(nums1, nums2))
+}
+
+// 寻找两个正序数组的中位数, 困难
+// 给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
+// 要求算法的时间复杂度为O(log (m+n))
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	totalLength := len(nums1) + len(nums2)
+	if totalLength%2 == 1 {
+		midIndex := totalLength / 2
+		return float64(getKthElement(nums1, nums2, midIndex+1))
+	} else {
+		midIndex1, midIndex2 := totalLength/2-1, totalLength/2
+		return float64(getKthElement(nums1, nums2, midIndex1+1)+getKthElement(nums1, nums2, midIndex2+1)) / 2.0
+	}
+}
+
+func getKthElement(nums1, nums2 []int, k int) int {
+	index1, index2 := 0, 0
+	for {
+		if index1 == len(nums1) {
+			return nums2[index2+k-1]
+		}
+		if index2 == len(nums2) {
+			return nums1[index1+k-1]
+		}
+		if k == 1 {
+			return min(nums1[index1], nums2[index2])
+		}
+		half := k / 2
+		newIndex1 := min(index1+half, len(nums1)) - 1
+		newIndex2 := min(index2+half, len(nums2)) - 1
+		pivot1, pivot2 := nums1[newIndex1], nums2[newIndex2]
+		if pivot1 <= pivot2 {
+			k -= newIndex1 - index1 + 1
+			index1 = newIndex1 + 1
+		} else {
+			k -= newIndex2 - index2 + 1
+			index2 = newIndex2 + 1
+		}
+	}
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
