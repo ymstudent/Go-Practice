54f2b6cc9eae8a7b24fc4f7e46b08188923edb9f
yanming <254500312@qq.com>
2022-09-26 23:30:51 +0800

    链表是否有环

---
+++ leetcode/leetcode/editor/cn/[141]环形链表.go
@@ -0,0 +1,24 @@
+package cn
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+	slow, fast := head, head
+	//注意条件：需要判断fast和fast.Next是否为空，否则会有空指针
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ leetcode/leetcode/editor/cn/[142]linked-list-cycle-ii.go
@@ -0,0 +1,33 @@
+package cn
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func detectCycle(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			slow = head
+			for slow != fast {
+				slow = slow.Next
+				fast = fast.Next
+			}
+			return fast
+		}
+	}
+	return nil
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
