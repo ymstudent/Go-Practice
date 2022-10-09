efce63a147b8f8fe8ccf057cdb99851648588fa5
yanming <yanming2@tal.com>
2022-10-09 15:02:54 +0800

    利用双指针实现2数之和

---
+++ leetcode/leetcode/editor/cn/[167]two-sum-ii-input-array-is-sorted.go
@@ -0,0 +1,19 @@
+package cn
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func twoSum(numbers []int, target int) []int {
+	lo, hi := 0, len(numbers)-1
+	for lo < hi {
+		sum := numbers[lo] + numbers[hi]
+		if sum < target {
+			lo++
+		} else if sum > target {
+			hi--
+		} else if sum == target {
+			return []int{lo + 1, hi + 1}
+		}
+	}
+	return []int{}
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
