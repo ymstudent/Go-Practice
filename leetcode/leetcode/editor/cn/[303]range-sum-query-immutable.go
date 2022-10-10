994d9fa34e26b89283373ee7df47ba2dc41be066
yanming <yanming2@tal.com>
2022-10-10 18:57:13 +0800

    利用前缀和技巧解决区域和检索问题，easy

--- leetcode/leetcode/editor/cn/[303]range-sum-query-immutable.go
+++ leetcode/leetcode/editor/cn/[303]range-sum-query-immutable.go
@@ -1,24 +1,21 @@
 package cn
 
-import "fmt"
-
 //leetcode submit region begin(Prohibit modification and deletion)
 type NumArray struct {
 	Arr []int
 }
 
-func Constructor(nums []int) NumArray {
+func Constructor2(nums []int) NumArray {
 	numArr := new(NumArray)
-	numArr.Arr = make([]int, len(nums) + 1)
+	numArr.Arr = make([]int, len(nums)+1) // ps1: 长度需要比原数组长多1位，首位用0填充，相当于链表的哑节点
 	for i := 1; i < len(numArr.Arr); i++ {
-		numArr.Arr[i] = numArr.Arr[i - 1] + nums[i - 1]
+		numArr.Arr[i] = numArr.Arr[i-1] + nums[i-1]
 	}
-	fmt.Println(numArr.Arr)
 	return *numArr
 }
 
 func (this *NumArray) SumRange(left int, right int) int {
-	return this.Arr[right + 1] - this.Arr[left]
+	return this.Arr[right+1] - this.Arr[left] // ps2: 用right + 1 -left
 }
 
 /**
