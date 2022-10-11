38dcb9239ffe8a8be99ba16d7b407f490fb2b6cf
yanming <yanming2@tal.com>
2022-10-11 10:33:25 +0800

    前缀和解决二维区域和检索--矩阵不可变问题， medium

---
+++ leetcode/leetcode/editor/cn/[304]range-sum-query-2d-immutable.go
@@ -0,0 +1,33 @@
+package cn
+
+//leetcode submit region begin(Prohibit modification and deletion)
+type NumMatrix struct {
+	preSum [][]int
+}
+
+func Constructor(matrix [][]int) NumMatrix {
+	m, n := len(matrix), len(matrix[0])
+	numMatrix := new(NumMatrix)
+	numMatrix.preSum = make([][]int, m+1)
+	numMatrix.preSum[0] = make([]int, n+1)
+	for i := 1; i <= m; i++ {
+		if len(numMatrix.preSum[i]) == 0 {
+			numMatrix.preSum[i] = make([]int, n+1)
+		}
+		for j := 1; j <= n; j++ {
+			numMatrix.preSum[i][j] = numMatrix.preSum[i-1][j] + numMatrix.preSum[i][j-1] + matrix[i-1][j-1] - numMatrix.preSum[i-1][j-1]
+		}
+	}
+	return *numMatrix
+}
+
+func (this *NumMatrix) SumRegion(row1 int, col1 int, row2 int, col2 int) int {
+	return this.preSum[row1][col1] + this.preSum[row2+1][col2+1] - this.preSum[row1][col2+1] - this.preSum[row2+1][col1]
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * obj := Constructor(matrix);
+ * param_1 := obj.SumRegion(row1,col1,row2,col2);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
