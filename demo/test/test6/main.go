093435a8d55ffe4c78eca65630bf0afbf5395839
yanming <254500312@qq.com>
2023-03-03 23:32:11 +0800

    interface test

---
+++ demo/test/test6/main.go
@@ -0,0 +1,69 @@
+package main
+
+import "fmt"
+
+type Pson interface {
+	howOld() int
+	growUp()
+}
+
+type Person struct {
+	age int
+}
+
+func (p Person) howOld() int {
+	return p.age
+}
+
+func (p *Person) growUp() {
+	p.age += 1
+}
+
+func main() {
+	// qcrao 是值类型
+	var qcrao Pson = &Person{age: 18}
+
+	// 值类型 调用接收者也是值类型的方法
+	fmt.Println(qcrao.howOld())
+
+	// 值类型 调用接收者是指针类型的方法
+	qcrao.growUp()
+	fmt.Println(qcrao.howOld())
+
+	// ----------------------
+
+	// stefno 是指针类型
+	var stefno Pson = &Person{age: 100}
+
+	// 指针类型 调用接收者是值类型的方法
+	fmt.Println(stefno.howOld())
+
+	// 指针类型 调用接收者也是指针类型的方法
+	stefno.growUp()
+	fmt.Println(stefno.howOld())
+
+	var c coder = &Gopher{"Go"}
+	c.code()
+	c.debug()
+
+	var c2 = Gopher{"Go"}
+	c2.code()
+	c2.debug()
+}
+
+type coder interface {
+	code()
+	debug()
+}
+
+type Gopher struct {
+	language string
+}
+
+func (p Gopher) code() {
+	fmt.Printf("I am coding %s language\n", p.language)
+}
+
+func (p *Gopher) debug() {
+	fmt.Printf("I am debuging %s language\n", p.language)
+}
